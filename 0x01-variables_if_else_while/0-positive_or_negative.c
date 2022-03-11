c94945fb9c18d8738453d5ee8f2f3102c0387449
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-03-11 15:03:05 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /*betty style doc for function main goes there */
+
+/**
+* main-Entry point
+*
+* retur: Always 0 (success
+*/
 int main(void)
 {
 	int n;
@@ -14,7 +20,7 @@ int main(void)
 	
 	if (n>0)
 	   printf("%d is positive\n", n);
-	elseif (n==0)
+	else if (n==0)
 	   printf("%d is zero\n", n);
 	else
 	   printf("%d is negative\n", n);
