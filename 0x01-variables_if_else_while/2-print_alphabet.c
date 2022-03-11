25f124b41cffe955fe7b3c0ba9f7ce12de24f4da
Roboto <ujlog01@gmail.com>
2022-03-11 12:59:38 +0000

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main(void)
+{
+	int ch;
+	clrscr();
+
+/* print alphabet in lowercase letters */
+	
+	for (ch= "a" ; ch<= "z" ; ch++)
+		putchar(ch);
+		putchar("\n");
+		getch();
+}
