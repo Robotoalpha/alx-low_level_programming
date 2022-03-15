ec114fa6115631cea023fb26c9e922ea3c6871cd
Roboto <ujlog01@gmail.com>
2022-03-15 14:54:58 +0000

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ * print_alphabet_x10-abcd
+   Description of function print_alphabet_x10:
+ * this function print alphabet in lower case ten times
+ * 
+ *  Return: Always retur 0 for success
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+	while (c <= 'z')
+	{
+		while (i <= 10)
+		{
+		_putchar(c);
+		c++;
+		i++;
+		}
+	}
+	_putchar('\n');
+}
