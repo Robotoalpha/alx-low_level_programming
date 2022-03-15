5b0cd85df55f5daf207a5d3314e07e1c34e0a41d
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-03-15 20:03:54 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ int print_sign(int n)
 int result;
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 result = 1;
 }
 else if ( n == 0)
 {
-putchar('0');
+_putchar('0');
 result = 0;
 }
 else
 {
-putchar('-');
-result = -1
+_putchar('-');
+result = -1;
 }
 return (result);
 }
