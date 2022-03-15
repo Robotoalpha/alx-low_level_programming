447e5a57c15bcbaefbb4e977f7987bb0e4b0dd85
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-03-15 15:57:43 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet_x10-abcd
- * Description of function print_alphabet_x10:
- * this function print alphabet in lower case ten times
- * 
- *  Return: Always return 0 for success
+ *print_alphabet_x10 - print alpha x10
+ *@void: no arguments.
+ *
+ *Return: no return value.
  */
 void print_alphabet_x10(void)
 {
 char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-int i , j;
- j = 0;
-while (j <= 26)
-{
+int i, j;
 i = 0;
-while (i <= 10)
+while (i < 10)
+{
+j = 0;
+while (j < 26)
 {
 _putchar(alphabet[j]);
 j++;
