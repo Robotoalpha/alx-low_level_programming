561f5b934b22cd93dffc006a2a3ed1a3828a5a20
Roboto <ujlog01@gmail.com>
2022-03-15 20:37:28 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * function _abs-calcul la valeur absolue
+ * d'un entier n
+ *
+ * Return : 0 always success
+ */
+int _abs(int)
+{
+int n;
+int valeurAbsolue;
+if (n > 0)
+{
+valeurAbsolue == n;
+return (valeurAbsolue);
+}
+else if (n == 0);
+[
+valeurAbsolue == 0;
+return (valeurAbsolue);
+}
+else
+{
+valeurAbsolue = n * (-1);
+return (valeurAbsolue);
+}
+}
+
+
