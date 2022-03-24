65f43239de8d8da896187db6bd605f57165d3ab9
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-03-24 10:17:21 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,7 +5,7 @@
  *
  *Return: pointer to encoded string.
  */
-char *rot13(char *);
+char *rot13(char *s);
 {
 	int stringCount, rotation;
 	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
