dbe7bc3d42e8b3f5f420b00c2c7c5d654203d33b
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-04-04 13:05:16 +0000

    frees a 2 dimensional grid previously created

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,48 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 /**
- * alloc_grid - create a matrix using malloc
- * @width: width of the matrix
- * @height: height of the matrix
- *
- * Return: return a pointer of pointer or null
- */
-int **alloc_grid(int width, int height)
+  * free_grid - frees a 2D grid created in alloc_grid.
+  * @grid: pointer to pointer.
+  * @height: height of grid.
+  *
+  * Return: void.
+  */
+void free_grid(int **grid, int height)
 {
-	int i, j, a, b;
-	int **p;
+	int i;
 
-	if (width <= 0 || height <= 0)
+	for (i = 0; i < height; i++)
 	{
-		return (NULL);
+		free(grid[i]);
 	}
-	else
-	{
-		p = (int **) malloc(height * sizeof(int *));
-		/* we have to make a malloc per pointer*/
-		if (!p)
-		{
-			free(p);
-			return (NULL);
-		}
-		for (i = 0; i < height; i++)
-		{
-			p[i] = (int *) malloc(width * sizeof(int));
-			if (!p[i])
-			{
-				for (j = 0; j <= i; j++)
-					free(p[j]);
-				free(p);
-				return (NULL);
-			}
-		}
-		for (a = 0; a < height; a++)
-		{
-			for (b = 0; b < width; b++)
-			{
-				p[a][b] = 0;
-			}
-		}
-		return (p);
-	}
+	free(grid);
 }
