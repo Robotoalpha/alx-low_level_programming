f1eb23490a16bf67ece2548fa2513920cc8f178d
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-04-04 13:34:34 +0000

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,111 +1,107 @@
+/*
+ * File: 100-strtow.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
 /**
- * wordnos - counts no of words in a given string
- * @str: pointer to the string
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
  *
- * Return: No. of words in the string (int)
+ * Return: The index marking the end of the initial word pointed to by str.
  */
-int wordnos(char *str)
+int word_len(char *str)
 {
-	int wordno, i, j;
+	int index = 0, len = 0;
 
-	wordno = 0;
-	i = 0;
-	while (*(str + i) != '\0')
+	while (*(str + index) && *(str + index) != ' ')
 	{
-		if (*(str + i) != 32 && *(str + i) != '\0')
-		{
-			j = i;
-			while (*(str + j) != 32 && *(str + j) != '\0')
-				j++;
-			wordno++;
-			i = j - 1;
-		}
-		i++;
+		len++;
+		index++;
 	}
-	return (wordno);
+
+	return (len);
 }
 
 /**
- * cpystr - copies words in string to different elements of 2d array of strings
- * @s: double pointer to a 2D array of strings
- * @str: pointer to string whose words are to be copied
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
  *
- * Return: void
+ * Return: The number of words contained within str.
  */
-void cpystr(char **s, char *str)
+int count_words(char *str)
 {
-	int i, j, l, idx;
+	int index = 0, words = 0, len = 0;
 
-	i = 0;
-	idx = 0;
-	while (*(str + i) != '\0')
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
 	{
-		if (*(str + i) != 32 && *(str + i) != '\0')
+		if (*(str + index) != ' ')
 		{
-			j = i;
-			l = 0;
-			while (*(str + j) != 32 && *(str + j) != '\0')
-			{
-				s[idx][l] = *(str + j);
-				l++;
-				j++;
-			}
-			s[idx][l] = '\0';
-			idx++;
-			i = j;
+			words++;
+			index += word_len(str + index);
 		}
-		i++;
 	}
+
+	return (words);
 }
 
 /**
- * strtow - splits a string into words and stores the words in an array
- * @str: pointer to string
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
  *
- * Return: double pointer to the array containing the words
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
  */
 char **strtow(char *str)
 {
-	char **s;
-	int wordno, i, j, k, length, idx;
+	char **strings;
+	int index = 0, words, w, letters, l;
 
 	if (str == NULL || str[0] == '\0')
-		return (0);
-	wordno = wordnos(str);
-	s = (char **)malloc(sizeof(char *) * (wordno + 1));
-	if (s == 0 || wordno == 0)
-		return (0);
-	i = 0;
-	idx = 0;
-	while (*(str + i) != '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
 	{
-		if (*(str + i) != 32 && *(str + i) != '\0')
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
 		{
-			j = i;
-			length = 0;
-			while (*(str + j) != 32 && *(str + j) != '\0')
-			{
-				length++;
-				printf("Length is %d\n", length);
-				j++;
-			}
-			*(s + idx) = (char *)malloc(sizeof(char) * (length + 1));
-			if (*(s + idx) == 0)
-			{
-				for (k = 0; k < idx; k++)
-					free(*(s + k));
-				free(s);
-				return (0);
-			}
-			idx++;
-			i = j - 1;
-			printf("value of i is %d \n", i);
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
 		}
-		i++;
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
 	}
-	cpystr(s, str);
-	return (s);
+	strings[w] = NULL;
+
+	return (strings);
 }
