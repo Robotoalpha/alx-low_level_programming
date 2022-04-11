f1fd10ec25c9828f54ed0a77e606e63c09083152
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-04-11 14:24:10 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,8 @@
 /*
  *File: 3-function_like_macro.h
  *Author: Ouattara Alphonse
- *Description: Macro ABS(x) Header file that defines a 
- *function-like macro ABS(x)
+ *Description:ABS(x) Header file that defines
+ *a function-like macro ABS(x)
  *that computes the absolute value of a number x.
  */
 
